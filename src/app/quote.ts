f1f344ae7bb302a07b5c38662bced6c0b22ebece
Floride125-art <FTuyisenge120@daviscollege.com>
2021-02-22 00:41:32 +0200

    adding everything I will need in my project to my class quote

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,15 @@
 export class Quote {
+    showDescription: boolean;
+    constructor(
+        public id: number,
+        public quote: any,
+        public author: string,
+        public publisher: string,
+        public completeDate: Date,
+        public upvote:number,
+        public downvote:number
+        )
+        {
+         this.showDescription=false;
+    }
 }
