d61a071bf09e33067bfd95b3d9bcad1ecf626593
Floride125-art <FTuyisenge120@daviscollege.com>
2021-02-22 01:26:09 +0200

    added pipe for upvote and downvote quotes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { FormQuotesComponent } from './form-quotes/form-quotes.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     FormQuotesComponent,
-    QuoteDetailsComponent
+    QuoteDetailsComponent,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
